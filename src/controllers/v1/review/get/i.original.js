f867bd5777e19e8ef6554bd8364a0aba1a4f5758
coder371 <abdalstar371371@gmail.com>
2024-11-14 14:35:05 +0200

    save

--- src/controllers/v1/review/get/i.original.js
+++ src/controllers/v1/review/get/i.original.js
@@ -1,11 +1,25 @@
 const getFile = require('../../../../services/aws/readFile');
+const crypto = require('crypto');
+
 module.exports = async (req, res, next) => {
   res.removeHeader('Transfer-Encoding');
   res.removeHeader('X-Powered-By');
   const { file } = req.params;
+
   try {
     const data = await getFile({ filePath: `uploads/images/${file}` });
+    const etag = crypto.createHash('md5').update(data).digest('hex');
+
+    // تحقق إذا كان الـ ETag الخاص بالملف هو نفسه المرسل من الـ client لتجنب إعادة تحميل الملف
+    if (req.headers['if-none-match'] === etag) {
+      return res.status(304).end(); // يرسل استجابة 304 - Not Modified
+    }
+
+    // إعداد الكاشينج
     res.setHeader('Content-Type', 'image/webp');
+    res.setHeader('Cache-Control', 'public, max-age=31536000'); // عام ولمدة سنة
+    res.setHeader('ETag', etag);
+
     res.send(data);
   } catch (error) {
     console.error("Error retrieving file from S3:", error);
